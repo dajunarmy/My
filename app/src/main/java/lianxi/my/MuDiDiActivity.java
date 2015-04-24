138d672d4b1308253c14e622efe27f2c86dfd13f
dajun <email@1208319934@qq.com>
2015-04-24 17:14:11 +0800

    改名了

--- app/src/main/java/lianxi/my/MuDiDiActivity.java
+++ app/src/main/java/lianxi/my/MuDiDiActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity2 extends ActionBarActivity {
+public class MuDiDiActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
